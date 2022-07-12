d947d957465bef25cbf16ccb130b09d3eb686e97
nilaarasu <108974025+nilaarasu@users.noreply.github.com>
2022-07-12 16:16:36 +0530

    Create DAY3_PROG7.c

---
+++ DAY3/DAY3_PROG7.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int age_year,month;
+	int birth_year,birth_month;
+	int current_year,current_month;
+	printf("enter birth year and month\n");
+	scanf("%d%d",&birth_month,&birth_year);
+	printf("enter current year and month\n");
+	scanf("%d%d",&current_month,&current_year);
+	if(birth_month>current_month)
+	{
+    age_year=(current_year-birth_year)-1;
+    printf("age in year:%d\n",age_year);
+    month=(12-birth_month)+current_month;
+    printf("age in month:%d\n",month);
+    return 0;
+	}
+	else
+	{
+	age_year=current_year-birth_year;
+	printf("age in year:%d\n",age_year);
+	month=current_month-birth_month;
+	printf("age in month:%d\n",month);
+	}
+}
+
+
