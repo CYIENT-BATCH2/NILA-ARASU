73b9cb8a2b7d724061c858d746efd3b37ef2d222
nilaarasu <108974025+nilaarasu@users.noreply.github.com>
2022-07-13 12:42:22 +0530

    Create DAY4_PROG(B).c

---
+++ DAY4/DAY4_PROG(B).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int x=10,y=20,z;
+	z=x<y;
+	printf("z=%d\n",z);
+	z=x>y;
+	printf("z=%d\n",z);
+	z=x<=y;
+	printf("z=%d\n",z);
+	z=(x*x-y)>(y*y-x);
+	printf("%d\n",z);
+	z=(x*x*x)<(y*x*x);
+	printf("z=%d\n",z);
+	z=((x*y)!=(y*x));
+	printf("z=%d\n",z);
+	z=((x*y)==(y*x));
+	printf("z=%d\n",z);
+
+
+}
